561069e3ebe49d23dad11c0c00987816ef97dd8f
linhongbin <522181941@qq.com>
2018-02-28 21:31:31 +0800

    finnal of HW2

--- Network Programming/HW2/Client.py
+++ Network Programming/HW2/Client.py
@@ -1,44 +1,53 @@
 import socket
 import sys
-import json
+import logging
 
-import os
-# create an INET TCP socket
 
 
 def main():
     soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     Wait_Flag = False
+    
+    
+    # Setting logger
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+    ch = logging.StreamHandler()
+    formatter = logging.Formatter(
+        "[%(asctime)s] [%(levelname)s] [%(processName)s] [%(threadName)s] :  %(message)s")
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
 
     # connect to the server (change localhost to an IP address if necessary)
     while True:
         try:
             if len(sys.argv) is not 4:
-                print("you should set arguments, server name:localhost ,port number:55703.")
+                logger.error("you should set arguments, server name:localhost ,port number:55703.")
                 return 0
             else:
-                soc.connect(("localhost", 55703))
-                print("Connected to server.")
+                soc.connect((sys.argv[1], int(sys.argv[2])))
+                logger.info("Connected to server at (%s, %s)" % (sys.argv[1], sys.argv[2]))
                 break
         except ConnectionRefusedError:
             if Wait_Flag is False:
-                print('Cannot connect to server at localhost:55703')
+                logger.error("Cannot Connected to server at (%s, %s)" % (sys.argv[1], sys.argv[2]))
                 return 0
 
     # Send a message to the server
 
-    with open(sys.argv[3]) as f:
-        msg = f.read()
-    msg = msg + "END"
-    print(msg)
+    msg = sys.argv[3]
+    msg = msg + "[END]"
+    logger.info(msg)
 
     msg = msg.encode("utf-8")
     soc.send(msg)
+    logger.info("URL sent to the server")
 
     # Receive data from the server
-    data = soc.recv(1024)
-    data = json.loads(data)
-    print(data)
+    rec = soc.recv(1024)
+    rec = rec.decode('utf-8')
+    rec_list = rec.split(';')
+    logger.info("Server response: (\'%s\', %f)" % (rec_list[0], float(rec_list[1])))
 
     # Always close the socket after use
     soc.close()
--- Network Programming/HW2/Server.py
+++ Network Programming/HW2/Server.py
@@ -1,97 +1,115 @@
+
 import socket
 import sys
-from multiprocessing import Process, Queue
+from multiprocessing import Process, Queue, Lock, Value
 import threadpool
 import logging
 from urllib import request
+import os
+import numpy as np
+from keras_squeezenet import SqueezeNet
+from keras.applications.imagenet_utils import preprocess_input, decode_predictions
+from keras.preprocessing import image
+import tensorflow as tf
 
-import json
-import nltk
-import ipdb
 
 
 
 def download(url, i):
     request.urlretrieve(url, "%d.jpg" % i)
 
-def multiProcess(i, connect_queue):
+def multiProcess(connect_queue, graph, pic_num, lock, process_id):
 
     MAX_THREADING_NUM = 4
     pool = threadpool.ThreadPool(MAX_THREADING_NUM)
 
+    try:
+        while(True):
+            (q_socket, q_address) = connect_queue.get()
+            lock.acquire()
+            _pic_num = pic_num.value + 1
+            pic_num.value =  _pic_num
+            lock.release()
 
-    logger = logging.getLogger(str(i))
-    formatter = logging.Formatter(
-        "  [%(levelname)s] : %(message)s")
-    logger.setLevel(logging.DEBUG)
-    ch = logging.StreamHandler()
-    logger.addHandler(ch)
-    ch.setFormatter(formatter)
-
-
-
-    logger.info("go into while loop")
-    while(True):
-        if not connect_queue.empty():
-            #logger.info("queue.empty, %d", connect_queue.empty())
-            print("creating thread")
-            (q_socket, q_address) = connect_queue.get(block=True)
-            dic_vars = {'process_id': i, 'client_socket': q_socket, 'address': q_address}
-            reqs = threadpool.makeRequests(server_worker, args_list=[(dic_vars,None),])
+            dic_vars = {'client_socket': q_socket, 'address': q_address, 'tf_graph':graph, 'pic_num':_pic_num, }
+            #print("get the address %s %s"%(q_address[0], q_address[1]))
+            reqs = threadpool.makeRequests(server_worker, args_list=[(None, dic_vars),])
             for req in reqs:
                 pool.putRequest(req)
+    except:
+        print("counter except, exit the process-%d" %(process_id))
+        pool.joinAllDismissedWorkers()
 
 
 
 
 
-def server_worker(process_id, client_socket, address):
-    logger = logging.getLogger(__name__)
-    formatter = logging.Formatter(
-        "Process %(i)s thread%(name)s, [%(levelname)s] : %(message)s")
+def server_worker(client_socket, address, tf_graph, pic_num):
+
+    logger = logging.getLogger('logger'+str(pic_num))
     logger.setLevel(logging.DEBUG)
     ch = logging.StreamHandler()
-    logger.addHandler(ch)
+    formatter = logging.Formatter(
+        "[%(asctime)s] [%(levelname)s] [%(processName)s], [%(threadName)s] :  %(message)s")
     ch.setFormatter(formatter)
+    logger.addHandler(ch)
 
-    print("go into thread")
+    logger.info("Recieved Client ('%s', %s)." % (address[0], address[1]))
+
     recieve_string = ""
 
     # Read data from a client and send it response back
     while True:
+        #print(client_socket)
         data = client_socket.recv(2048)
         data_decode = data.decode(("utf-8"))
         recieve_string = recieve_string + data_decode
-
         # All the message from client is recieved completely
-        if recieve_string[-3:] == 'END':
-            recieve_string = recieve_string[:-3]
-            tokens = nltk.word_tokenize(recieve_string)
-            tagged = nltk.pos_tag(tokens)
-            print(recieve_string)
-            json_data = json.dumps(tagged)
-            # print("json %s  " % json_data)
-            json_data = json_data.encode('utf-8')
-            client_socket.sendall(json_data)
-            client_socket.close()
-            print("Client disconnected.")
+        if recieve_string.find("[END]") != -1:
             break
+    recieve_string= recieve_string[:-5]
+    logger.info("Client submitted URL %s" % recieve_string)
+    request.urlretrieve(recieve_string, "images/%d.jpg" % pic_num)
+
+    logger.info("Image saved to images/%d.jpg" %(pic_num))
+    with tf_graph.as_default():
+        model = SqueezeNet()
+        image_str = "images/" + str(pic_num) + ".jpg"
+        img = image.load_img(image_str, target_size=(227, 227))
+        x = image.img_to_array(img)
+        x = np.expand_dims(x, axis=0)
+        x = preprocess_input(x)
+        preds = model.predict(x)
+        result =  decode_predictions(preds)
+        #print('Predicted:', result)
+        result = result[0][0]
+
+    logger.info("SqueezeNet result: (\"%s\", %.3f)" % (result[1], result[2]))
+    result = [result[1], str(result[2])]
+    result_string = ';'.join(result)
+    client_socket.sendall(result_string.encode("utf-8"))
+    client_socket.shutdown(socket.SHUT_RDWR)
+    client_socket.close()
+    logger.info("Client connection closed")
+    del logger
+
 
 
 def main():
 
     PROCESS_NUM = 4
     THREADING_NUM = 4
+    LISTENING_NUM = 20
 
 
     process_list = []
 
     # Setting logger
     logger = logging.getLogger(__name__)
-    formatter = logging.Formatter(
-        "%(name)s, [%(levelname)s] : %(message)s")
-    logger.setLevel(logging.INFO)
+    logger.setLevel(logging.DEBUG)
     ch = logging.StreamHandler()
+    formatter = logging.Formatter(
+        "[%(asctime)s] [%(levelname)s] [%(processName)s] [%(threadName)s] :  %(message)s")
     ch.setFormatter(formatter)
     logger.addHandler(ch)
 
@@ -99,26 +117,37 @@ def main():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     # parsing the argument
-    if len(sys.argv) is not 2:
-        print("you should set arguments, port number:")
+    if len(sys.argv) is not 3:
+        logger.error("you should set arguments, port number, the number of processor:")
 
     try:
         server_socket.bind(('localhost', int(sys.argv[1])))
+        PROCESS_NUM = int(sys.argv[2])
     except:
-        print("Cannot set server localhost:%s" % sys.argv[1])
+        logger.error("Cannot set server localhost:%s" % sys.argv[1])
         return 0
 
     # Control the number of client in queue when listening
-    server_socket.listen(20)
-    print("Listening for incoming connections on port %s" % sys.argv[1])
+    server_socket.listen(LISTENING_NUM)
+    logger.info("Start listening for connections on port %s" % sys.argv[1])
 
     # A Global queue with connect clients
     connect_queue = Queue()
+    lock = Lock()
+    pic_num = Value('i', 0)
+
+    graph = tf.get_default_graph()
+
     # Create Multi-Process
     for i in range(PROCESS_NUM):
-        p = Process(target=multiProcess, args=(i, connect_queue,))
+        p = Process(target=multiProcess, args=(connect_queue, graph, pic_num, lock, i, ))
+        logger.info("Created process Process-%d" % (i + 1))
         p.start()
-        process_list.append(p)
+
+
+    if not (os.path.isdir("images/")):
+        os.mkdir('images')
+
 
 
     # A indefinite loop
@@ -127,8 +156,7 @@ def main():
 
         (client_socket, address) = server_socket.accept()
         connect_queue.put((client_socket, address))
-        logger.info("queue number %d", connect_queue.qsize())
-        logger.info("Client %s connected" % address[0])
+        logger.info("Client ('%s', %s) connected." % (address[0], address[1]))
 
 if __name__ == '__main__':
     main()
